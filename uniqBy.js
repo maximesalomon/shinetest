6330c1cd23d2ac499a8672c87556e532ace0f473
Maxime Salomon <maxime@croissant.io>
2019-09-05 23:20:02 +0200

    uniqBy

--- uniqBy.js
+++ uniqBy.js
@@ -1,1 +1,24 @@
 // TO DO
+
+const uniqBy = (key, arr) => {
+    const acc = []
+    arr.map(x => {
+        function checkKey(accValue) {
+            return accValue[key] === x[key]
+        }
+        const existsInAcc = acc.filter(checkKey).length > 0
+        if(existsInAcc === false) {
+            acc.push(x)
+        } else {
+            const idxdbl = acc.findIndex(checkKey)
+            acc[idxdbl] = x
+        }
+    })
+    return acc
+}
+
+console.log(uniqBy('id', [
+    { id: 1, name: 'Anakin', age: 10 },
+    { id: 1, name: 'Darth Vader', side: 'dark' },
+    { id: 2, name: 'Anakin' },
+]))
